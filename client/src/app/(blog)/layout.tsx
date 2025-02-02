081768615403d432fb4d2f7c82bdc5bb1dfd1498
mohyware <mohieelden50@gamil.com>
2025-02-02 19:15:27 +0200

    refactor: some fixes in client side

--- client/src/app/(blog)/layout.tsx
+++ client/src/app/(blog)/layout.tsx
@@ -7,26 +7,23 @@ export default function Layout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <div
+            className={`antialiased min-h-screen bg-white text-slate-900  ${inter.className}`}
+        >
+            <div className="max-w-2xl mx-auto py-10 px-4">
+                <header>
 
-            <body
-                className={`antialiased min-h-screen bg-white text-slate-900  ${inter.className}`}
-            >
-                <div className="max-w-2xl mx-auto py-10 px-4">
-                    <header>
-
-                        <div className="flex items-center justify-between">
-                            <nav className="ml-auto text-lg font-medium space-x-6">
-                                <Link href="/blog">Blog</Link>
-                                <Link href="/admin/blog">Dashboard</Link>
-                                <Link href="/about">About</Link>
-                            </nav>
-                        </div>
-                    </header>
-                    <br />
-                    <main>{children}</main>
-                </div>
-            </body>
-        </html>
+                    <div className="flex items-center justify-between">
+                        <nav className="ml-auto text-lg font-medium space-x-6">
+                            <Link href="/blog">Blog</Link>
+                            <Link href="/admin/blog">Dashboard</Link>
+                            <Link href="/about">About</Link>
+                        </nav>
+                    </div>
+                </header>
+                <br />
+                <main>{children}</main>
+            </div>
+        </div>
     );
 }
